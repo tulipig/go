0757975703e21739370fae4ea0131ce3f387d9ea
tulip.mac <epinghuang@163.com>
2019-07-15 00:10:05 +0800

    add

---
+++ github.com/user/tcpserver/echo2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "time"
+    "bufio"
+    "strings"
+    "net"
+)
+
+func main() {
+    listenner,err := net.Listen("tcp","localhost:8000")
+    if err!=nil{
+        log.Fatal(err)
+    }
+
+    for{
+        conn,err := listenner.Accept()
+        if err!=nil{
+            log.Print(err)
+            continue
+        }
+        handleConn(conn)
+        //go handleConn(conn)
+    }
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+    fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+    time.Sleep(delay)
+    fmt.Fprintln(c, "\t", shout)
+    time.Sleep(delay)
+    fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+    input := bufio.NewScanner(c)
+    for input.Scan() {
+        go echo(c, input.Text(), 1*time.Second)
+    }
+    // NOTE: ignoring potential errors from input.Err()
+    c.Close()
+}
