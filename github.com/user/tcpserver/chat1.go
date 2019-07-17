1ed75e756f320add5461eacbd1f83869b6d29f0c
tulip.mac <epinghuang@163.com>
2019-07-17 22:33:50 +0800

    add

---
+++ github.com/user/tcpserver/chat1.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "log"
+    "bufio"
+)
+
+func main() {
+    listener,err := net.Listen("tcp","localhost:8000")
+    if err!=nil{
+        log.Fatal(err)
+    }
+
+    go broadfast()
+
+    for{
+        conn,err := listener.Accept()
+        if err!=nil{
+            log.Print(err)
+            continue
+        }
+
+        go handleConn(conn)
+    }
+}
+
+type client chan<- string
+var(
+    entering = make(chan client)
+    leaving = make(chan client)
+    messages = make(chan string)
+)
+
+func broadfast() {
+    clients := make(map[client]bool)
+    for{
+        select{
+        case msg := <-messages:
+            // Broadcast incoming message to all
+            // clients' outgoing message channels.
+            for cli := range clients{
+                cli <- msg
+            }
+        case cli := <-entering:
+            clients[cli] = true
+        case cli := <-leaving:
+            delete(clients,cli)
+            close(cli)
+        }
+    }
+}
+
+func handleConn(conn net.Conn) {
+    ch := make(chan string)
+    go clientWriter(conn, ch)
+
+    who := conn.RemoteAddr().String()
+    ch <- "You are " + who
+    messages <- who + " have arrived"
+    entering <- ch
+
+    input := bufio.NewScanner(conn)
+    for input.Scan(){
+        messages <- who + ": " + input.Text()
+    }
+
+    // NOTE: ignoring potential errors from input.Err()
+
+    leaving <- ch
+    messages <- who + " has left"
+    conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+    for msg := range ch{
+        fmt.Fprintln(conn,msg) //NOTE: ignoring network errors
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
